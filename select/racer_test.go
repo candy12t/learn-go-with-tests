d0ed3446d947542e32a9d5af962637e8e778b386
Takumi Kanada <candy12t@gmail.com>
2021-11-14 00:15:50 +0900

    Add http test with `httptest`

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,8 +1,23 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
+
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
 	slowURL := "http://wwww.facebook.com"
 	fastURL := "http://wwww.quii.co.uk"
 
@@ -12,4 +27,7 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
