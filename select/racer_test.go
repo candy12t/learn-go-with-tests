e0592b1d1850fc8ea4b4903e1ff2599adca34697
Takumi Kanada <candy12t@gmail.com>
2021-11-14 00:35:53 +0900

    Add test

--- select/racer_test.go
+++ select/racer_test.go
@@ -9,21 +9,18 @@ import (
 
 func TestRacer(t *testing.T) {
 
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0 * time.Millisecond)
+	t.Run("return an error if a server doesn't respond within 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer serverA.Close()
+		defer serverB.Close()
+		_, err := Racer(serverA.URL, serverB.URL)
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
-
-	want := fastURL
-	got := Racer(slowURL, fastURL)
-
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
