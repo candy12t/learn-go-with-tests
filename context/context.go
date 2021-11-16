d42fbd58a2154d80189b2d939a70b3dfda320546
Takumi Kanada <candy12t@gmail.com>
2021-11-17 01:30:16 +0900

    Update context, weather or not store canceled test code

--- context/context.go
+++ context/context.go
@@ -3,14 +3,31 @@ package context
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 type Store interface {
 	Fetch() string
+	Cancel()
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		store.Cancel()
 		fmt.Fprint(w, store.Fetch())
 	}
+}
+
+type SpyStore struct {
+	response  string
+	cancelled bool
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
 }
--- context/context_test.go
+++ context/context_test.go
@@ -1,9 +1,11 @@
 package context
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 type StubStore struct {
@@ -14,16 +16,37 @@ func (s *StubStore) Fetch() string {
 	return s.response
 }
 
+func (s *StubStore) Cancel() {}
+
 func TestHandler(t *testing.T) {
 	data := "hello, world"
-	svr := Server(&StubStore{data})
+	// svr := Server(&StubStore{data})
 
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
+	// request := httptest.NewRequest(http.MethodGet, "/", nil)
+	// response := httptest.NewRecorder()
 
-	svr.ServeHTTP(response, request)
+	// svr.ServeHTTP(response, request)
 
-	if response.Body.String() != data {
-		t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
-	}
+	// if response.Body.String() != data {
+	// 	t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+	// }
+
+	t.Run("tells store to canel work if request is canceled", func(t *testing.T) {
+		store := &SpyStore{response: data}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if !store.cancelled {
+			t.Errorf("store was nto told to cancel")
+		}
+	})
 }
