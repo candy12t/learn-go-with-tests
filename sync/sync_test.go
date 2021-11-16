2a9b5f366d2349468f7394c8295b9baeb24944aa
Takumi Kanada <candy12t@gmail.com>
2021-11-17 00:52:48 +0900

    Update sync, use sync package to control order goroutine

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,6 +1,9 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter9h(t *testing.T) {
 	t.Run("incremeting the counter 3 times leaves it at 3", func(t *testing.T) {
@@ -10,6 +13,24 @@ func TestCounter9h(t *testing.T) {
 		counter.Inc()
 
 		assertCounter(t, counter, 3)
+	})
+
+	t.Run("int runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Inc()
+				w.Done()
+			}(&wg)
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
 	})
 }
 
